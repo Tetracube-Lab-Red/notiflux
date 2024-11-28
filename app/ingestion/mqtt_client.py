49ad35138e7a8ac56ee852b95fc3f20aacc2f87f
Davide Di Domenico <davide.didomenico@targatelematics.com>
2024-11-28 16:34:57 +0100

    🥙 Adds MQTT client, observing startup events, logging

---
+++ app/ingestion/mqtt_client.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+
+from app import logger
+
+_mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    logger.info(f"Connected with result code {reason_code}")
+    client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    logger.info(msg.topic+" "+str(msg.payload))
+
+def start_consume():
+    logger.info("Starting to consume devices telemetries")
+    _mqttc.on_connect = on_connect
+    _mqttc.on_message = on_message
+    _mqttc.connect("mqtt.eclipseprojects.io", 1883, 60)
+    _mqttc.loop_start()
+
+def stop_consume():
+    logger.info("Stop to consume devices telemetries")
+    _mqttc.disconnect()
--- app/main.py
+++ app/main.py
@@ -1,9 +1,26 @@
+from contextlib import asynccontextmanager
 from typing import Union
-
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.ingestion import mqtt_client
+from app import logger
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Custom startup operations")
+    mqtt_client.start_consume()
+    yield
+    logger.info("Custom shutdown operations")
+    mqtt_client.stop_consume()
+
+    
+app = FastAPI(
+    title="Notiflux",
+    description="Make something useful with TetraCube.Red Platform data",
+    version="1.0.0",
+    lifespan=lifespan
+)
 
 @app.get("/")
 def read_root():
