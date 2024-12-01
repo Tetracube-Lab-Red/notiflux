bc1e212ba8a786022e59cf4d1fc42a2d87ba93b4
Davies <davide.dcube@gmail.com>
2024-12-01 15:59:52 +0100

    🏗️ Rule engine build in progress

--- app/database/entities.py
+++ app/database/entities.py
@@ -10,6 +10,7 @@ class Alert(SQLModel, table=True):
     id: Optional[uuid.UUID] = Field(default_factory=uuid.uuid4, primary_key=True)
     device_type: DeviceType = Field(sa_column=Column(Enum(DeviceType), nullable=False))
     device_slug: str = Field(nullable=False)
-    field_reference: str = Field(nullable=False)
+    field: str = Field(nullable=False)
+    alert_type: str = Field(nullable=False)
     open_event_ts: datetime = Field(nullable=False)
     close_event_ts: datetime = Field(nullable=True)
--- app/engine/rule_engine.py
+++ app/engine/rule_engine.py
@@ -8,6 +8,22 @@ from app.clients import ups_pulsar_api_client as ups_api
 from app.engine.rules_scripts_loader import rules_scripts
 
 
+async def open_new_alert(evaluation_dict: dict):
+    logger.info('Check if there is another open alert of the same type')
+    logger.info('Storing the alert')
+    await send_firebase_notification()
+
+
+async def close_old_alert(evaluation_dict: dict):
+    logger.info('Check if there is any open alert of the same type')
+    logger.info('Closing the alert')
+    await send_firebase_notification()
+
+
+async def send_firebase_notification():
+    logger.info('Notify the app for the notification')
+
+
 async def run_rules(device_type: DeviceType, device_name: str):
     logger.info(f"Gatering devices information for {device_name}")
     if device_type not in rules_scripts:
@@ -21,12 +37,13 @@ async def run_rules(device_type: DeviceType, device_name: str):
                 device_telemetry = await ups_api.get_telemetry(device_name)
             except Exception as e:
                 logger.error(f"Cannot retrieve telemetry data for error {e}")
+                return
 
     for script in rules_scripts[device_type]:
-        result = script.evaluate(device_telemetry)
-
-    alert = Alert(device_slug="test", device_type=DeviceType.UPS,
-                  field_reference="input_voltage", open_event_ts=datetime.utcnow())
-    session = next(get_database(), None)
-    session.add(alert)
-    session.commit()
+        evaluation_dict = script.evaluate(device_telemetry)
+        if 'valid' in evaluation_dict and evaluation_dict['valid'] == True:
+            await close_old_alert(evaluation_dict)
+        elif 'valid' in evaluation_dict and evaluation_dict['valid'] == False:
+            await open_new_alert(evaluation_dict)
+        else:
+            logger.error(f'Invalid evaluation response: {evaluation_dict}')
